bbf05d766b93f5176c56fd384a83c2e951ed0735
rubanokSem <qwe7818197@gmail.com>
2023-07-22 15:04:43 +0300

    новый файл:    1.rb

---
+++ 1.rb
@@ -0,0 +1,14 @@
+puts ("vvedi N")
+chisloN = gets.chomp
+n = chisloN.to_i
+puts ("vvedi M")
+chisloM = gets.chomp
+m = chisloM.to_i
+puts ("poshel nahui") if n <= 0 or m <= 0
+result = n 
+sum =0 
+while result <= n do 
+    result += sum 
+    result += n 
+end
+puts (sum)
